9c69681945a454a97ed1415f9551230910095c0a
Sameer Ahuja <sameer.ahuja@gmail.com>
2022-03-21 17:59:04 -0700

    Wait a little longer.

--- youtube/show-transcript.user.js
+++ youtube/show-transcript.user.js
@@ -3,11 +3,13 @@
 // @namespace   sa
 // @match       http*://www.youtube.tld/watch?v=*
 // @grant       none
-// @version     1.0
+// @run-at      document-idle
+// @version     1.1
 // @author      Sameer Ahuja
 // @description On pageload, click the sequence of buttons necessary to load the video transcript.
 // ==/UserScript==
 
-setTimeout(() => document.querySelector('[aria-label="More actions"]').click(), 1000)
+// The delays are arbitrary to give YouTube enough time to load its resources.
+setTimeout(() => document.querySelector('[aria-label="More actions"]').click(), 3000)
 // TODO: This is brittle and will fail when YouTube makes any changes to the order of the list items.
-setTimeout(() => document.querySelectorAll('tp-yt-paper-listbox ytd-menu-service-item-renderer')[1].click(), 2000)
+setTimeout(() => document.querySelectorAll('tp-yt-paper-listbox ytd-menu-service-item-renderer')[1].click(), 4000)
